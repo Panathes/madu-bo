56f6e8101eda798d331d4ac1d47178d75ef155b1
Panathes <dianketohead@gmail.com>
2020-01-30 09:45:04 +0100

    add Switch Step

--- src/components/questionnr.jsx
+++ src/components/questionnr.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Layout, Menu, Icon, Input, Button, Radio, Table, Switch, Form, Divider, Tag } from 'antd';
+import { Layout, Menu, Icon, Input, Button, Radio, Table, Switch, Form, Divider, Tag, Steps, message } from 'antd';
 import 'antd/dist/antd.css';
 
 const { Header, Sider, Content } = Layout;
@@ -7,8 +7,31 @@ const { Header, Sider, Content } = Layout;
 
 function Questionnr() {
   const [collapsed, setCollapsed] = useState(false);
+  const [current, setCurrent] = useState(0);
 
   const { Search } = Input;
+  const { Step } = Steps;
+  
+
+  const steps = [
+    {
+      title: 'First',
+      content: 'First-content',
+    },
+    {
+      title: 'Second',
+      content: 'Second-content',
+    },
+    {
+      title: 'Third',
+      content: 'Third-content',
+    },
+    {
+      title: 'Last',
+      content: 'Last-content',
+    },
+  ];
+
 
     return (
         <Layout>
@@ -46,6 +69,31 @@ function Questionnr() {
               minHeight: 840,
             }} 
           >
+            <div>
+              <Steps current={current}>
+                {steps.map(item => (
+                  <Step key={item.title}/>
+                ))}
+              </Steps>
+              <div className="steps-content">{steps[current].content}</div>
+              <div className="steps-action">
+                {current < steps.length - 1 && (
+                  <Button type="primary" onClick={() => setCurrent(current + 1)}>
+                    Next
+                  </Button>
+                )}
+                {current === steps.length - 1 && (
+                  <Button type="primary" onClick={() => message.success('Processing complete!')}>
+                    Done
+                  </Button>
+                )}
+                {current > 0 && (
+                  <Button style={{ marginLeft: 8 }} onClick={() => setCurrent(current - 1)}>
+                    Previous
+                  </Button>
+                )}
+              </div>
+            </div>          
           </Content>
         </Layout>
       </Layout>
